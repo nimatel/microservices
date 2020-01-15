62b5fbbcdd5937de420c31dda1e30d3e554b0e59
Ivan Zupancic <ivan.zupancic@dlab.si>
2020-01-15 22:58:44 +0100

    pebkac

--- working/main.go
+++ working/main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
-	gh := handlers.Goodbye(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
